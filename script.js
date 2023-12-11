038b7b438c3e44ffbf574ebc52e151f7a67e3f51
MairaAl <122545402+MairaAl@users.noreply.github.com>
2023-12-11 15:28:26 -0300

    Corrección de errores

--- script.js
+++ script.js
@@ -1,49 +1,69 @@
+let volumen;
 let boton = document.getElementById("calcular");
 let dia = document.getElementById("dia");
 let flu = document.getElementById("flu");
 let man = document.getElementById("man");
 let error = document.getElementById("error");
 let input = document.getElementById("peso");
-boton.addEventListener("click", calcular);
-input.addEventListener("keyup", calcular);
 
-function calcular() {
-  const DATO = document.getElementById("peso").valueAsNumber;
-  console.log(DATO);
-  if (DATO > 0) {
-    error.style.display = "none";
-    let valor = segar(DATO);
-    console.log(valor);
-    let mantenimiento = valor * 1.5;
-    let porHora = valor / 24;
-    dia.innerHTML = "Diario: " + valor + " cc";
-    flu.innerHTML = " Por hora: " + Math.round(porHora) + " cc/hr";
-    man.innerHTML = "m+m/2: " + mantenimiento + " cc";
-    flu.style.display = "block";
-    man.style.display = "block";
-    dia.style.display = "block";
-    function segar(peso) {
-      if (DATO > 20) {
-        return (DATO - 20) * 20 + 1500;
-      } else if (DATO > 10) {
-        return (DATO - 10) * 50 + 1000;
-      } else {
-        return DATO * 100;
-      }
-    }
-    function superficie(peso) {
-      return;
-      (DATO * 4 + 7) / (DATO + 90);
-    }
-    if (DATO <= 30) {
-      segar(peso);
-    } else {
-      superficie(peso);
-    }
+boton.addEventListener("click", () => {
+  flu.style.display = "none";
+  man.style.display = "none";
+  error.style.display = "none";
+  dia.style.display = "none";
+
+  let peso = document.getElementById("peso").valueAsNumber;
+  if (peso > 0) {
+    calcularPeso(peso);
   } else {
     error.style.display = "block";
     flu.style.display = "none";
     man.style.display = "none";
     dia.style.display = "none";
   }
+});
+
+function calcularPeso(peso) {
+  if (peso <= 30) {
+    volumen = segar(peso);
+    dia.innerHTML = "Diario: " + volumen + " cc";
+    dia.style.display = "block";
+    flu.innerHTML = "Mantenimiento: " + Math.floor(volumen / 24) + " cc/hr";
+    flu.style.display = "block";
+    man.innerHTML = "m+m/2: " + Math.floor(volumen / 24) * 1.5 + " cc/hr";
+    man.style.display = "block";
+  } else {
+    Volumen = superficie(peso);
+    dia.innerHTML =
+      "*1500: " +
+      Math.floor(volumen * 1500) +
+      " cc" +
+      "/" +
+      "*2000: " +
+      Math.floor(volumen * 2000) +
+      " cc";
+    dia.style.display = "block";
+    flu.innerHTML =
+      "Man: " +
+      Math.floor((volumen * 1500) / 24) +
+      " cc/hr" +
+      " - " +
+      Math.floor((volumen * 2000) / 24) +
+      " cc/hr";
+    flu.style.display = "block";
+  }
+}
+function segar(peso) {
+  if (peso > 20) {
+    volumen = (peso - 20) * 20 + 1500;
+  } else if (peso > 10) {
+    volumen = (peso - 10) * 50 + 1000;
+  } else {
+    volumen = peso * 100;
+  }
+  return volumen;
+}
+function superficie(peso) {
+  volumen = (peso * 4 + 7) / (peso + 90);
+  return volumen;
 }
